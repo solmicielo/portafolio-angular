b25b432d7a4ef7641d63005c78957a7399598f54
solmicielo <carreterosoledad@gmail.com>
2022-10-09 21:03:38 -0300

    dividido en componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { FormacionComponent } from './componentes/formacion/formacion.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CabeceraComponent,
+    ExperienciaComponent,
+    FormacionComponent,
+    SkillsComponent,
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
